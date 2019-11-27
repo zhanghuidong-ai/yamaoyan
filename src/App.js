ad8a42f9c46c4aa27734a88f42cd9a849a4a6bf9
Z-H-D <zhanghuidongnb.@163.com>
2019-11-27 09:22:43 +0800

    框架搭建

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import "./assets/reset.css"
+import {Route,Switch}  from "react-router-dom"
+import Home from "./views/Home"
+import My from "./views/My";
+// import Cinema from "./views/Cinema";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">	
+      <Switch>
+		<Route path={"/my"} component={My}></Route>
+      	<Route path={"/"}  component={Home}></Route>
+      </Switch>
     </div>
   );
 }
